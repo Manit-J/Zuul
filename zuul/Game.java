fb5d414d7d59faccd2485aa0e89186e8e0c99c15
Manit Jawa <100145444+Manit-J@users.noreply.github.com>
2023-06-20 20:07:11 -0400

    Update Game.java

--- zuul/Game.java
+++ zuul/Game.java
@@ -3,8 +3,7 @@ import java.util.Stack;
 /**
  *  This class is the main class of the "World of Zuul" application. 
  *  "World of Zuul" is a very simple, text based adventure game.  Users 
- *  can walk around some scenery. That's all. It should really be extended 
- *  to make it more interesting!
+ *  can walk around some scenery, visit different rooms and interact with some items.
  * 
  *  To play this game, create an instance of this class and call the "play"
  *  method.
